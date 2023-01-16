91f251e07c20c653bbf3288bf2606d5c13c4090a
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2023-01-16 13:47:13 -0300

    refactor: interfaces-input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-
-interface iInput {
-  name: string;
-  type?: string;
-}
+import { iInput } from "../../interfaces";
 
 const Input = ({ name, type }: iInput) => {
   return (
---
+++ src/interfaces/index.ts
@@ -0,0 +1,4 @@
+export interface iInput {
+    name: string;
+    type?: string;
+  }
